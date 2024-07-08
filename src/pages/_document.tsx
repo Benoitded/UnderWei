fb32ae828c29b9b5526a4b136c18b97c33e73ac6
Mf <38403795+DeBelg@users.noreply.github.com>
2024-07-08 12:21:55 +0200

    update header css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* <title>Supabase</title> */}
-        <link rel="icon" href="/ClariFi.png" />
+        <link rel="icon" href="./app/favicon.ico" />
       </Head>
       <body>
         <Main />
